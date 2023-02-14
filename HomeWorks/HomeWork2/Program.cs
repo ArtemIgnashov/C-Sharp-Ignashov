7754e74b92528b9be729042134e583c199384034
Artem <8ignash8@gmail.com>
2023-02-14 17:07:20 +0300

    2

--- HomeWorks/HomeWork2/Program.cs
+++ HomeWorks/HomeWork2/Program.cs
@@ -3,4 +3,7 @@
 Console.WriteLine("Напишите трёхзначное число и на выходе покажу вторую цифру этого числа");
 
 int num = Convert.ToInt32(Console.ReadLine());
-
+int cur = num/10;
+int cur1 = cur%10;
+ 
+Console.WriteLine(cur1);
