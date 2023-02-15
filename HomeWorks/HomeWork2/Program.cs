6c5c88f0653d6fbd7d745bcbb2ab67e9e3247300
Artem <8ignash8@gmail.com>
2023-02-15 19:56:47 +0300

    123

--- HomeWorks/HomeWork2/Program.cs
+++ HomeWorks/HomeWork2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 10
 
+/*
 Console.WriteLine("Напишите трёхзначное число и на выходе покажу вторую цифру этого числа");
 
 int num = Convert.ToInt32(Console.ReadLine());
@@ -7,3 +8,29 @@ int cur = num/10;
 int cur1 = cur%10;
  
 Console.WriteLine(cur1);
+
+*/
+
+//Задача 2
+
+Console.WriteLine("Напиши число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//12345/10=1234
+//12345%10=5
+//int leng1= num.Length;
+
+
+void Fill(int[] coll)
+
+{
+    int length = coll.Length;
+    int index = 0;
+    while (index < length)
+    {
+        coll[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+Console.WriteLine(num);
