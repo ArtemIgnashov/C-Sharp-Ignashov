7992c0de64fb61dbd9c3ca49353414cb715e37d2
Artem <8ignash8@gmail.com>
2023-02-17 17:26:12 +0300

    123

--- Seminars/Seminar 3/Program.cs
+++ Seminars/Seminar 3/Program.cs
@@ -20,13 +20,13 @@ GetDiapozon(numqadrant);
 // Задача 1
 // Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
 //выдаёт номер четверти плоскости, в которой находится эта точка.
-
+/*
 void qadrant(double x, double y)
 {
-    if (x > 0 & y > 0) Console.WriteLine("qadrant = 1");
-    else if (x < 0 & y > 0) Console.WriteLine("qadrant = 2");
-    else if (x < 0 & y < 0) Console.WriteLine("qadrant = 3");
-    else if (x > 0 & y < 0) Console.WriteLine("qadrant = 4");
+    if (x > 0 && y > 0) Console.WriteLine("qadrant = 1");
+    else if (x < 0 && y > 0) Console.WriteLine("qadrant = 2");
+    else if (x < 0 && y < 0) Console.WriteLine("qadrant = 3");
+    else if (x > 0 && y < 0) Console.WriteLine("qadrant = 4");
     else Console.WriteLine("Wrong input!");
 }
 
@@ -38,10 +38,49 @@ double y = Convert.ToDouble(Console.ReadLine());
 
 qadrant(x,y);
 
-
+*/
 // Задача 2
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+/*
+void Power(double Num)
+{
+    double counter = 1;
+    double print = 0;
+    while (counter <= Num)
+    {
+        print = Math.Pow(counter, 2);
+        Console.Write(print + " ");
+        counter++;
+
+    }
+    
+}
+
+Console.WriteLine("Введите число: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Power(y);
+
+*/
 
 //Задача 3
 //Напишите программу, которая принимает на вход координаты двух точек 
 //и находит расстояние между ними в 2D пространстве.
+
+double hypotenuse(double xa, double ya, double xb, double yb)
+{
+    return Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+}
+
+Console.WriteLine("последовательно введите координаты точек: ");
+Console.Write("xa: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("ya: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("xb: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("yb: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Длинна отрезка равна " + Math.Round(hypotenuse(xa, ya, xb, yb),2));
+
