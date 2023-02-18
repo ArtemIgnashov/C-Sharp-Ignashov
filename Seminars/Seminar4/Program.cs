58a3d10b15616cc6658073f314934f3c050aee3f
Artem <8ignash8@gmail.com>
2023-02-18 18:55:00 +0300

    4

---
+++ Seminars/Seminar4/Program.cs
@@ -0,0 +1,5 @@
+﻿int Getsum (int num)
+{
+    int sum = 0;
+    for (int cuurent = 0; current <= num; current ++ )                        // инициализация рабочей переменной index / условие существования цикла/
+}                                //ихменение рабочей переменной
