fae35757edbd4d9d3919f9e3be531c3b7bac9764
Artem <8ignash8@gmail.com>
2023-02-21 16:57:12 +0300

    Sem5 objective4

--- Seminars/Seminar 5/Program.cs
+++ Seminars/Seminar 5/Program.cs
@@ -193,7 +193,7 @@ Srowarray(ChangeArray1);*/
 
 //Задача 3 
 //Написать программу, определяющую, присутствует ли заданное число в массиве.
-
+/*
 int[] CreateArray(int size) //метод для ввода данный вручную
 {
     int[] array = new int[size];
@@ -207,13 +207,16 @@ int[] CreateArray(int size) //метод для ввода данный вруч
 
 void quest(int[] array, int n)// метод поиска числа в массиве
 {
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] == n)
         {
             Console.WriteLine($"элемент под номером {i + 1} равен искомому числу: {n} ");
+            count++;
         }
     }
+    if (count == 0) Console.WriteLine("искомого элемента нет");
 }
 
 void Srowarray(int[] array) // метод для вывода на экран массива
@@ -234,12 +237,63 @@ Console.WriteLine("введите искомое число:  ");
 int Num = Convert.ToInt32(Console.ReadLine());
 
 
-quest(newArray,Num);
-
-
+quest(newArray, Num);
+*/
 
 //Задача 4
 //Задать массив из n элементов, определить кол-во элементов, принадлежащих отрезку [a,b].
+
+int[] CreateRondomArray(int size, int minValue, int maxValue) // создали метод для создания рандомного массива
+{
+    int[] array = new int[size]; // выделить память под массив размером size
+
+    for (int index = 0; index < size; index++)
+        array[index] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void Srowarray(int[] array) // метод для вывода на экран массива
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(+array[i] + " ");
+    Console.WriteLine();
+}
+
+void quest(int[] array, int a, int b)// определить кол-во элементов, принадлежащих отрезку [a,b]
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= b && array[i] >= a)
+        {
+    Console.WriteLine($"элемент [ {array[i]} ] под номером {i + 1} входит в диапазон: [{a}, {b}]");
+    count++;
+}
+    }
+    if (count == 0) Console.WriteLine($"элементов входящих в диапазон: [{a}, {b}] не найдено");
+}
+
+
+Console.WriteLine("введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите минимальвый элемент:  ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите максимальный элемент:  ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRondomArray(size, min, max);
+Console.WriteLine("Массив: ");
+Srowarray(newArray);
+
+Console.WriteLine("введите A:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите B:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+quest(newArray,A,B);
 
 
 
