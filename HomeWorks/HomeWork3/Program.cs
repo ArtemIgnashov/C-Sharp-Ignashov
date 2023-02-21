a9e854f5479aec93cdd6f7f55d5a595c824556f1
Artem <8ignash8@gmail.com>
2023-02-21 11:49:41 +0300

    3hw

--- HomeWorks/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19 
+// Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+
+double function(double num1)
+{
+    double curr = 10;
+    double y = 1;
+    double num2 = (num1 % curr)*(Math.Pow(10,((num1.ToString().Length)-y)));
+
+
+    while (num2.ToString().Length <= num1.ToString().Length)
+    {
+        num2 = num2 + (num1 % curr)*(Math.Pow(10,((num1.ToString().Length)-y)));
+        curr = curr * 10;
+        y--; 
+        Console.WriteLine(num1 + "   " + num2);
+    }
+return num2;
+
+
+
+}
+
+
+Console.WriteLine("введите число мы проверим полиндром ли оно: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(function(num2));
+
+
+//num1.ToString().Length);
+
+
