7c8f7aa67af76fd87d8f2029e407693bfc379597
Artem <8ignash8@gmail.com>
2023-02-21 13:36:59 +0300

    HW3

--- HomeWorks/HomeWork3/Program.cs
+++ HomeWorks/HomeWork3/Program.cs
@@ -43,8 +43,53 @@ Console.WriteLine(result);
 Console.WriteLine(polin(Num, result));
 */
 
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+/*
+double hypotenuse(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    return Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2)+ Math.Pow(za - zb, 2));
+}
 
+Console.WriteLine("последовательно введите координаты точек: ");
 
+Console.Write("xa: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("ya: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("za: ");
+double za = Convert.ToDouble(Console.ReadLine());
 
+Console.Write("xb: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+Console.Write("yb: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("zb: ");
+double zb = Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine("Длинна отрезка равна " + Math.Round(hypotenuse(xa, ya, za, xb, yb, zb),2));
+*/
+
+//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void cube (double num)
+{
+    double counter = 1;
+    double i = 0;
+    while (counter <=num)
+    {
+        i=Math.Pow(counter, 3);
+        Console.Write(i+" ");
+        counter++;
+    }
+    
+}
+
+Console.WriteLine("введите число: ");
+
+double Num = Convert.ToDouble(Console.ReadLine());
+
+cube(Num);
