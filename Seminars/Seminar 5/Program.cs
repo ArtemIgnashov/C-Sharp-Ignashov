9adaa4da24946a0ef01634bbe284da240c4563bc
Artem <8ignash8@gmail.com>
2023-02-21 14:36:38 +0300

    sem5

---
+++ Seminars/Seminar 5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Семинар 5 
+// работа с масивами
+
+//Задача 1 
+//Написать программу, которая определяет сумму отрицательных элементов в массиве.
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        array[i] = N;
+    }
+    return array;
+}
