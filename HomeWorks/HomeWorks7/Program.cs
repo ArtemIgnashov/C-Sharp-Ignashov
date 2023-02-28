810a3d06f78411dafe77ff90db229a657a784b10
Artem <8ignash8@gmail.com>
2023-02-28 20:51:15 +0300

    HW7 task 50,47,52?

--- HomeWorks/HomeWorks7/Program.cs
+++ HomeWorks/HomeWorks7/Program.cs
@@ -79,7 +79,7 @@ ShowDoubleArray2D(resultarray);
 //элемента в двумерном массиве, и возвращает значение этого элемента 
 //или же указание, что такого элемента нет.
 
-
+/*
 int[,] CreateRandon2dArray() //метод генирации массива 2D
 {
     Console.WriteLine("введите кол-во строк:  ");
@@ -146,7 +146,133 @@ if (task(newArray, num1, num2))
     Console.WriteLine($"элемент под номером ({num1 + 1};{num2 + 1}) = {result}");
 }
 else Console.WriteLine($"Элемент ({num1 + 1};{num2 + 1}) не входит в массив ");
+*/
 
 //Задача 52. Задайте двумерный массив из целых чисел. 
 //Найдите среднее арифметическое элементов в каждом столбце.
-//else Console.WriteLine("Элемент не входит в массив ");
+
+/*
+int[,] CreateRandon2dArray() //метод генирации массива 2D
+{
+    Console.WriteLine("введите кол-во строк:  ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите кол-во столбцов:  ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите минимальное значение:  ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите максимальное значение:  ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int[,] array = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray2D(int[,] array) // метод вывода масива 2D
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] AverageValue(int[,] array1) // метод нахождения среднего арифметического в столбцах
+{
+    double[] average = new double[array1.GetLength(1)];
+    double[] sum = {0};
+    for (int i = 0; i < array1.GetLength(1); i++)
+        for (int j = 0; j < array1.GetLength(0); j++)
+            sum = sum + array1[j, i];
+    average = sum / array1.GetLength(0);
+
+    return average;
+}
+
+void Showarray(int[] array) // метод для вывода на экран массива
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(+array[i] + " ");
+    Console.WriteLine();
+}
+
+
+
+int[,] newArray = CreateRandon2dArray();
+ShowArray2D(newArray);
+
+int[] array = AverageValue(newArray);
+Showarray(array);
+*/
+
+
+int[,] CreateRandon2dArray() //метод генирации массива 2D
+{
+    Console.WriteLine("введите кол-во строк:  ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите кол-во столбцов:  ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите минимальное значение:  ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите максимальное значение:  ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int[,] array = new int[rows, colums];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray2D(int[,] array) // метод вывода масива 2D
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+double[] AverageValue(int[,] array1) // метод нахождения среднего арифметического в столбцах
+{
+    double[] average = new double[array1.GetLength(1)];
+    double[] sum = {0};
+    for (int i = 0; i < array1.GetLength(1); i++)
+        for (int j = 0; j < array1.GetLength(0); j++)
+            sum = sum + array1[j, i];
+    average = sum / array1.GetLength(0);
+
+    return average;
+}
+
+for (int i = 0; i < 4; i++)
+{
+    int summ = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        summ = summ + array[j, i];
+    }
+    double average = summ / 3;
+    int n = i + 1;
+    Console.WriteLine("Avarage for " + n + " column is " + average);
+}
+
+int[,] newArray = CreateRandon2dArray();
+ShowArray2D(newArray);
+
