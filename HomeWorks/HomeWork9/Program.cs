ba2bf438ce8f1ca2da0b94e465e734923d38be6c
Artem <8ignash8@gmail.com>
2023-03-03 19:19:52 +0300

    HW 9 task 64,66,68

--- HomeWorks/HomeWork9/Program.cs
+++ HomeWorks/HomeWork9/Program.cs
@@ -19,6 +19,7 @@ OutputOfNaturalNum(N);
 //Задача 66: Задайте значения M и N. Напишите программу, 
 //которая найдёт сумму натуральных элементов в промежутке от M до N.
 
+/*
 int SumNaturalElements(int m, int n)
 {
     if (m > n) return SumNaturalElements(m - 1, n) + m;
@@ -33,7 +34,26 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 int result = SumNaturalElements(M, N);
 Console.WriteLine($"суммa натуральных элементов в промежутке от M до N =  {result}");
+*/
 
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 //Даны два неотрицательных числа m и n.
 
+
+double Akkerman(double m, double n)
+{
+    double res = 0;
+    if (m == 0) return res = (n + 1);
+    else if (m > 0 & n == 0) res = Akkerman(m - 1, 1);
+    else if (m > 0 & n > 0) res = Akkerman(m - 1, Akkerman(m, n - 1));
+    return res;
+}
+
+
+Console.WriteLine("Задайте N: ");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Задайте N: ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+double res = Akkerman(m,n);
+Console.WriteLine($"result = {res} ");
