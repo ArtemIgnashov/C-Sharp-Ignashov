3a4118092b0dc99fddc42586adbd87fb5447bbd5
Artem <8ignash8@gmail.com>
2023-03-04 13:51:40 +0300

    HW 8

--- HomeWorks/HomeWork8/Program.cs
+++ HomeWorks/HomeWork8/Program.cs
@@ -3,7 +3,43 @@
 //которая упорядочит по убыванию элементы каждой строки двумерного массива.
 //Например, задан массив:
 
+int[,] CreateRandom2DArray()
+{
+    Console.WriteLine("введите кол-во строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
 
+    Console.WriteLine("введите кол-во столбцов: ");
+    int colums = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите миниальное значение: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("введите максимальное значение: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    int[,] array = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] newarray = CreateRandom2DArray();
+ShowArray2D(newarray);
 
 //Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
 //которая будет находить строку с наименьшей суммой элементов.
